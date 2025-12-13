cce0bcc4c3549322b2dd6aed9d9aafc29d0acb20
ysrastogi <ysrastogi19@gmail.com>
2025-12-13 14:33:52 +0530

    feat: Add __init__.py for Technical Indicators package

---
+++ feature_engine/indicators/__init__.py
@@ -0,0 +1,30 @@
+"""
+Technical Indicators Package
+"""
+
+from .base import BaseIndicator, PriceBasedIndicator, OHLCBasedIndicator, OHLCVBasedIndicator
+from .registry import IndicatorRegistry
+from .moving_averages import SMAIndicator, EMAIndicator, WMAIndicator, HMAIndicator, TEMAIndicator
+from .momentum import RSIIndicator, MACDIndicator, StochasticIndicator, WilliamsRIndicator, ROCIndicator, CCIIndicator
+from .volatility import ATRIndicator, ADXIndicator, BollingerBandsIndicator, SuperTrendIndicator
+from .volume import OBVIndicator
+
+__all__ = [
+    # Base classes
+    'BaseIndicator', 'PriceBasedIndicator', 'OHLCBasedIndicator', 'OHLCVBasedIndicator',
+
+    # Registry
+    'IndicatorRegistry',
+
+    # Moving Averages
+    'SMAIndicator', 'EMAIndicator', 'WMAIndicator', 'HMAIndicator', 'TEMAIndicator',
+
+    # Momentum
+    'RSIIndicator', 'MACDIndicator', 'StochasticIndicator', 'WilliamsRIndicator', 'ROCIndicator', 'CCIIndicator',
+
+    # Volatility
+    'ATRIndicator', 'ADXIndicator', 'BollingerBandsIndicator', 'SuperTrendIndicator',
+
+    # Volume
+    'OBVIndicator',
+]
